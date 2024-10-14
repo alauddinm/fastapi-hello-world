e4b1d56110124ee5d8493b302258253d06a7d33b
“alauddinm” <alauddinm@gmail.com>
2024-10-14 15:35:59 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World from Route 1!"}
+
+@app.get("/greet")
+def read_greet():
+    return {"message": "Hello, Greetings from Route 2!"}
+
