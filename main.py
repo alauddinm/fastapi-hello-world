e4721bb874938152900cfa9775a95c09548afec2
“alauddinm” <alauddinm@gmail.com>
2024-10-15 07:34:49 +0600

    route 3 added

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello, World from Route 1!"}
 
-
 @app.get("/greet")
 def read_greet():
     return {"message": "Hello, Greetings from Route 2!"}
+
+@app.get("/meet")
+def read_greet():
+    return {"message": "Hello, Greetings from Route 3!"}
 
